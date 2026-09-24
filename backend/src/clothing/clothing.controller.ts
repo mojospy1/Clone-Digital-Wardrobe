31d4927d739ccb5e1f731f840675f104d3379de8
mojospy1 <zackdeaths@gmail.com>
2026-09-24 08:31:53 +0700

    add Clothing CRUD controller

--- backend/src/clothing/clothing.controller.ts
+++ backend/src/clothing/clothing.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Clothing } from './clothing.entity';
+import { ClothingService } from './clothing.service';
 
 @Controller('clothing')
-export class ClothingController {}
+export class ClothingController {
+  constructor(private readonly clothingService: ClothingService) {}
+
+  @Post()
+  create(@Body() clothingData: Partial<Clothing>): Promise<Clothing> {
+    return this.clothingService.create(clothingData);
+  }
+
+  @Get()
+  findAll(): Promise<Clothing[]> {
+    return this.clothingService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<Clothing> {
+    return this.clothingService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() clothingData: Partial<Clothing>,
+  ): Promise<Clothing> {
+    return this.clothingService.update(id, clothingData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.clothingService.remove(id);
+  }
+}
