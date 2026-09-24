a0069de5bd361c4a05f5fbe063f5d9fea53472ed
mojospy1 <zackdeaths@gmail.com>
2026-09-24 08:25:12 +0700

    add Category CRUD controller

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Category } from './category.entity';
+import { CategoryService } from './category.service';
 
-@Controller('category')
-export class CategoryController {}
+@Controller('categories')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Post()
+  create(@Body() categoryData: Partial<Category>): Promise<Category> {
+    return this.categoryService.create(categoryData);
+  }
+
+  @Get()
+  findAll(): Promise<Category[]> {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<Category> {
+    return this.categoryService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() categoryData: Partial<Category>,
+  ): Promise<Category> {
+    return this.categoryService.update(id, categoryData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.categoryService.remove(id);
+  }
+}
