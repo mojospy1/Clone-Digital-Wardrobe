2ce330e8b9fd5a3e288dccce86d37926208653f1
mojospy1 <zackdeaths@gmail.com>
2026-09-24 08:33:31 +0700

    add OutfitItem service CRUD

--- backend/src/outfit-item/outfit-item.service.ts
+++ backend/src/outfit-item/outfit-item.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { OutfitItem } from './outfit-item.entity';
 
 @Injectable()
-export class OutfitItemService {}
+export class OutfitItemService {
+  constructor(
+    @InjectRepository(OutfitItem)
+    private readonly outfitItemRepository: Repository<OutfitItem>,
+  ) {}
+
+  create(itemData: Partial<OutfitItem>): Promise<OutfitItem> {
+    const item = this.outfitItemRepository.create(itemData);
+    return this.outfitItemRepository.save(item);
+  }
+
+  findAll(): Promise<OutfitItem[]> {
+    return this.outfitItemRepository.find({
+      relations: ['outfit', 'clothing'],
+    });
+  }
+
+  async findOne(outfit_item_id: number): Promise<OutfitItem> {
+    const item = await this.outfitItemRepository.findOne({
+      where: { outfit_item_id },
+      relations: ['outfit', 'clothing'],
+    });
+    if (!item) {
+      throw new NotFoundException(`Outfit item ${outfit_item_id} not found`);
+    }
+    return item;
+  }
+
+  async update(
+    outfit_item_id: number,
+    itemData: Partial<OutfitItem>,
+  ): Promise<OutfitItem> {
+    await this.findOne(outfit_item_id);
+    await this.outfitItemRepository.update(outfit_item_id, itemData);
+    return this.findOne(outfit_item_id);
+  }
+
+  async remove(outfit_item_id: number): Promise<void> {
+    const item = await this.findOne(outfit_item_id);
+    await this.outfitItemRepository.remove(item);
+  }
+}
