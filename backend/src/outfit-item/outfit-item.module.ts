e107b14c1cd9ccf0be05cf76cef24f89bc122b11
mojospy1 <zackdeaths@gmail.com>
2026-09-24 08:34:02 +0700

    register OutfitItem repository in module

--- backend/src/outfit-item/outfit-item.module.ts
+++ backend/src/outfit-item/outfit-item.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OutfitItemController } from './outfit-item.controller';
 import { OutfitItemService } from './outfit-item.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OutfitItem } from './outfit-item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([OutfitItem])],
   controllers: [OutfitItemController],
-  providers: [OutfitItemService]
+  providers: [OutfitItemService],
 })
 export class OutfitItemModule {}
