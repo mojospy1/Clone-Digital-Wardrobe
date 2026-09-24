1f34f1ffa8ef9067030a0b3f2cbc17ce9a20b3c5
mojospy1 <zackdeaths@gmail.com>
2026-09-24 08:19:08 +0700

    add User CRUD controller

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() userData: Partial<User>): Promise<User> {
+    return this.userService.create(userData);
+  }
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() userData: Partial<User>,
+  ): Promise<User> {
+    return this.userService.update(id, userData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.userService.remove(id);
+  }
+}
