7537e5ef612619b06dbbcd22570aa90a7af6bce5
mojospy1 <zackdeaths@gmail.com>
2026-09-24 08:10:26 +0700

    add User service CRUD

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  create(userData: Partial<User>): Promise<User> {
+    const user = this.userRepository.create(userData);
+    return this.userRepository.save(user);
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(user_id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({ user_id });
+    if (!user) {
+      throw new NotFoundException(`User ${user_id} not found`);
+    }
+    return user;
+  }
+
+  async update(user_id: number, userData: Partial<User>): Promise<User> {
+    await this.findOne(user_id);
+    await this.userRepository.update(user_id, userData);
+    return this.findOne(user_id);
+  }
+
+  async remove(user_id: number): Promise<void> {
+    const user = await this.findOne(user_id);
+    await this.userRepository.remove(user);
+  }
+}
